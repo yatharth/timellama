c840d654e7edf5b6dcb016d42a5d7bdb2f1aeab0
Yatharth Agarwal <yatharth999@gmail.com>
2021-08-25 17:37:08 -0700

    Improve MaxWidthLayout component (not really used anymore)

--- components/MaxWidthLayout.tsx
+++ components/MaxWidthLayout.tsx
@@ -1,7 +1,8 @@
 import {Children} from "./types";
 import {CSSProperties} from "react";
 
-function MaxWidthLayout({maxWidth = '30rem', verticalPadding, style, children}: { maxWidth?: string, verticalPadding?: string, style?: CSSProperties, children: Children }) {
+// It’s recommended to wrap this within a div that has horizontal padding and vertical padding set as needed.
+function MaxWidthLayout({maxWidth = '30rem', style, children}: { maxWidth?: string, style?: CSSProperties, children: Children }) {
     return <>
 
         <div style={style}>
@@ -13,9 +14,6 @@ function MaxWidthLayout({maxWidth = '30rem', verticalPadding, style, children}: 
               width: 100%;
               max-width: ${maxWidth};
               margin: 0 auto;
-              
-              padding-top: ${verticalPadding};
-              padding-bottom: ${verticalPadding};
             }
         `}</style>
 
